ec322f7e980d706f01c82f9c325631ea996b702f
maxmonteiro <maxmonteiro86@gmail.com>
2020-09-18 19:18:46 -0300

    Adicionados componentes vue base e rotas

--- meetings-frontend/src/router/index.js
+++ meetings-frontend/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import SignIn from '../views/SignIn.vue'
+import Users from '../views/Users.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +11,16 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/signin',
+    name: 'signin',
+    component: SignIn
+  },
+  {
+    path: '/users',
+    name: 'users',
+    component: Users
   }
 ]
 
---
+++ meetings-frontend/src/views/Users.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h2>Usuários</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'users',
+  components: {
+    
+  }
+}
+</script>
