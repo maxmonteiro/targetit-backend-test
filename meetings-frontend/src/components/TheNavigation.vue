292924c130afd30021823855ecec2fdbfb68a723
maxmonteiro <maxmonteiro86@gmail.com>
2020-09-18 19:26:57 -0300

    Alterado componente base para o menu

--- meetings-frontend/src/components/TheNavigation.vue
+++ meetings-frontend/src/components/TheNavigation.vue
@@ -1,5 +1,29 @@
 <template>
     <div>
-        nav
+        <ul>
+            <li>
+                <router-link :to="{ name: 'home' }">
+                    Home
+                </router-link>
+            </li>
+            <li>
+                <router-link :to="{ name: 'signin' }">
+                    Entrar
+                </router-link>
+            </li>
+            <li>
+                <router-link :to="{ name: 'users' }">
+                    Usuários
+                </router-link>
+            </li>
+            <li>
+                Máximo Henrique
+            </li>
+            <li>
+                <a href="#">
+                    Sair
+                </a>
+            </li>
+        </ul>
     </div>
 </template>
