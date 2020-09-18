e8ae3344acff544cb5a20313e39f3d286f1aaf7d
maxmonteiro <maxmonteiro86@gmail.com>
2020-09-18 18:45:04 -0300

    Criado método que invalida token ao realizar logout.

--- targetit-test/app/Http/Controllers/Auth/SignOutController.php
+++ targetit-test/app/Http/Controllers/Auth/SignOutController.php
@@ -7,8 +7,13 @@ use App\Http\Controllers\Controller;
 
 class SignOutController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware(['auth:api']);
+    }
+
     public function __invoke()
     {
-        dd('hello');
+        auth()->logout();
     }
 }
