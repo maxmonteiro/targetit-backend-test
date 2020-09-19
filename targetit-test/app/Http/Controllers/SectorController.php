6b8f88a930e5bfdf8017e91cfdf529abfa9cad2e
Maximo <maxmonteiro86@gmail.com>
2020-09-19 11:18:45 -0300

    Criado controller de setores

---
+++ targetit-test/app/Http/Controllers/SectorController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Sector;
+use Illuminate\Http\Request;
+use App\Http\Resources\Sector as SectorResource;
+
+class SectorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $sectors = Sector::all();
+        return SectorResource::collection($sectors);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $sector = Sector::create($request->all());
+        return new SectorResource($sector);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $sector = Sector::findOrFail($id);
+        return new SectorResource($sector);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $sector = Sector::findOrFail($id);
+        $sector->update($request->all());
+
+        return new SectorResource($sector);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $sector = Sector::findOrFail($id);
+        $sector->delete();
+
+        return new SectorResource($sector);
+    }
+}
--- targetit-test/routes/api.php
+++ targetit-test/routes/api.php
@@ -3,8 +3,9 @@
 Route::group(['prefix' => 'auth', 'namespace' => 'Auth'], function() {
     Route::post('signin', 'SignInController');
     Route::post('signout', 'SignOutController');
-    
+
     Route::get('me', 'MeController');
 });
 
 Route::apiResource('users', 'UserController');
+Route::apiResource('sectors', 'SectorController');
