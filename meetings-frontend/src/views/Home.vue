5b43202da08afbd8060596d923205c89e54aa0e7
Maximo <maxmonteiro86@gmail.com>
2020-09-21 11:19:15 -0300

    Criado formulario de agendamento. Selecionando data, local e sala

--- meetings-frontend/src/views/Home.vue
+++ meetings-frontend/src/views/Home.vue
@@ -15,16 +15,63 @@
           v-model="date" 
           :language="ptBR"
         ></datepicker>
-        <button class="bg-light text-primary" @click.stop="newScheduling">
+        <button class="bg-light text-primary" data-toggle="modal" data-target="#addNew" @click="newScheduling">
           <i class="fa fa-plus"></i> Novo
         </button>
       </div>
     </div>
 
+    <!-- Modal agendamento -->
+    <div
+      class="modal fade"
+      id="addNew"
+      tabindex="-1"
+      role="dialog"
+      aria-labelledby="addNewLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="addNewLabel">{{ !editMode ? 'Novo Agendamento' : 'Editar Agendamento' }}</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <form @submit.prevent="!editMode ? createScheduling() : updateScheduling()">
+            <div class="modal-body">
+                <label for="">Local</label>
+                <div class="form-group">
+                    <select type="text" name="local_id" class="form-control"
+                      v-model="selectedLocal"
+                      @change="getRooms"
+                    >
+                      <option value="">Selecione o local</option>
+                      <option v-for="local in locals" :key="local.id" :value="local.id">{{ local.name }}</option>
+                    </select>
+                </div>
+                <label for="">Sala</label>
+                <div class="form-group">
+                    <select type="text" name="room_id" class="form-control">
+                      <option value="">Selecione uma sala</option>
+                      <option v-for="room in rooms" :key="room.id" :value="room.id">{{ room.name }}</option>
+                    </select>
+                </div>
+            </div>
+            <div class="modal-footer">
+              <button id="closeModal" type="button" class="btn btn-default" data-dismiss="modal">Fechar</button>
+              <button type="submit" class="btn btn-primary">Salvar</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div><!-- ./Modal agendamento -->
+
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 import Datepicker from 'vuejs-datepicker'
 import { ptBR } from 'vuejs-datepicker/dist/locale'
 import moment from 'moment'
@@ -36,7 +83,14 @@ export default {
   data() {
     return {
       ptBR: ptBR,
-      date: ''
+      date: '',
+      locals: [],
+      selectedLocal: '',
+      rooms: [],
+      form: {
+        
+      },
+      editMode: false,
     }
   },
   filters: {
@@ -52,13 +106,37 @@ export default {
   mounted() {
     var dateToday = new Date()
     this.date = dateToday
+
+    this.getLocals()
   },
   methods: {
+    getLocals() {
+      axios.get('api/locals')
+      .then(({data}) => {
+        this.locals = data.data
+      }).catch((err) => {
+        console.log(err)
+      });
+    },
+    getRooms() {
+      let local = this.locals.find(elem => {
+        return elem.id == this.selectedLocal
+      })
+      this.rooms = local.rooms
+    },
     openDatepicker() {
       this.$refs.datepicker.showCalendar()
     },
     newScheduling() {
       console.log('novo agendamento')
+      this.editMode = false
+      this.showModal = true
+    },
+    createScheduling() {
+
+    },
+    updateScheduling() {
+
     }
   }
 }
@@ -77,7 +155,7 @@ export default {
   position: absolute;
   right: 0px;
   padding: 4px 12px;
-  font-size: 18px;
+  font-size: 16px;
   border: none;
   border-radius: 500px;
   outline: none;
